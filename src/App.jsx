6b93212a4b45642bb47ca9773dfccb75e3707fdc
itz-Hiru <hirumithagithub@gmail.com>
2025-05-09 09:59:08 +0530

    Created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home.page";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+      <Toaster
+        toastOptions={{
+          style: {
+            fontSize: "13px",
+          },
+        }}
+      />
+    </Router>
+  );
+};
 
-export default App
+export default App;
